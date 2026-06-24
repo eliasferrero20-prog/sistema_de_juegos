5bd345b5daa7d24b63ea2ca7226a735788d6f9e5
ramiro <ramirogarmendia2004@gmail.com>
2026-06-24 19:46:15 -0300

    mejora codigo ahorcado

--- ahorcado.py
+++ ahorcado.py
@@ -1,28 +1,29 @@
-palabra=input()
+import random
+palabra=["casa", "perro", "gato", "mesa", "silla", "ventana", "puerta", "escuela", "amigo", "pelota"]
+aleatorio=random.choice(palabra)
 palabra1=""
-for i in range(len(palabra)):
+for i in range(len(aleatorio)):
     palabra1+="-"
 print(palabra1)
 palabrasecreta=""
 intentos=5
-while intentos!=0:
+print("Pista (primer letra):", aleatorio[0])
+finalizado=False
+while intentos!=0 and finalizado==False:
     letra=input()
-    palabrasecreta="" 
-    intentos-=1 
-    for i in range(len(palabra)):
-        if letra==palabra[i]:
+    palabrasecreta=""
+    intentos-=1
+    for i in range(len(aleatorio)):
+        if letra==aleatorio[i]:
             palabrasecreta+=letra
+            intentos += 1
         else:
             palabrasecreta+=palabra1[i]
     palabra1=palabrasecreta
     print(palabrasecreta)
-if palabra==palabrasecreta:
+    if aleatorio == palabrasecreta:
+        finalizado=True
+if aleatorio==palabrasecreta:
     print("Ganaste")
 else:
     print("Perdiste")
-
-
-
-
-
-
