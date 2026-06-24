d4d4b873b50e34af6f755cecd7e2b8ccdf4d7360
Miqueas17 <miqueasbarrozo12@gmail.com>
2026-06-23 22:08:45 -0300

    agrege ramdon a mi juego

--- Piedra,PapeloTijeras.py
+++ Piedra,PapeloTijeras.py
@@ -1,22 +1,25 @@
+import random
+aleatorio=["piedra", "papel", "tijera" ]
+aleatorio2=random.choice(aleatorio)
 eleccion1=input("(Piedra, Papel o Tijera)")
-eleccion2=input("(Piedra, Papel o Tijera)")
-if eleccion1 == "piedra" and eleccion2 == "papel":
+print(aleatorio2)
+if eleccion1 == "piedra" and aleatorio2 == "papel":
     print("Ganador: Papel")
-elif eleccion1 == "tijera" and eleccion2 == "papel":
+elif eleccion1 == "tijera" and aleatorio2 == "papel":
     print("Ganador: Tijera")
-elif eleccion1 == "piedra" and eleccion2 == "tijera":
+elif eleccion1 == "piedra" and aleatorio2 == "tijera":
     print("Ganador: Piedra")
-elif eleccion1 == "papel" and eleccion2 == "piedra":
+elif eleccion1 == "papel" and aleatorio2 == "piedra":
     print("Ganador: Papel")
-elif eleccion1 == "papel" and eleccion2 == "tijera":
+elif eleccion1 == "papel" and aleatorio2 == "tijera":
     print("Ganador: Tijera")
-elif eleccion1 == "tijera" and eleccion2 == "piedra":
+elif eleccion1 == "tijera" and aleatorio2 == "piedra":
     print("Ganador: Piedra")
-elif eleccion1 == "tijera" and eleccion2 == "tijera":
+elif eleccion1 == "tijera" and aleatorio2 == "tijera":
     print("Empate")
-elif eleccion1 == "piedra" and eleccion2 == "piedra":
+elif eleccion1 == "piedra" and aleatorio2 == "piedra":
     print("Empate")
-elif eleccion1 == "papel" and eleccion2 == "papel":
+elif eleccion1 == "papel" and aleatorio2 == "papel":
     print("Empate")
 else :
     print("Dato ingresado no valido")
