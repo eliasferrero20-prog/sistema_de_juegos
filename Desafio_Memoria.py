b4c9368b6ad843c21b2e5bd7f55defb61d490677
ggrossidev <grossigon@gmail.com>
2026-06-24 03:02:53 -0300

    Desafio_Memoria incorporación funciones cartas y tablero

--- Desafio_Memoria.py
+++ Desafio_Memoria.py
@@ -1,3 +1,5 @@
+import random
+
 def leer_parejas(nombre_archivo):
 
     parejas = []
@@ -8,3 +10,43 @@ def leer_parejas(nombre_archivo):
             dato1, dato2 = linea.split(";")
             parejas.append((dato1, dato2))
     return parejas
+
+def generar_cartas(parejas):
+
+    cartas = []
+    id_pareja = 1
+    for i in parejas:
+        cartas.append((i[0], id_pareja))
+        cartas.append((i[1], id_pareja))
+        id_pareja += 1
+    return cartas
+
+def crear_tablero(cartas, filas, columnas):
+
+    cartas_mezcladas = []
+
+    while len(cartas) > 0:
+
+        posicion = random.randint(0, len(cartas) - 1)
+
+        cartas_mezcladas.append(cartas[posicion])
+
+        cartas.pop(posicion)
+
+    tablero = []
+
+    posicion = 0
+
+    for i in range(filas):
+
+        fila = []
+
+        for j in range(columnas):
+
+            fila.append(cartas_mezcladas[posicion])
+
+            posicion += 1
+
+        tablero.append(fila)
+
+    return tablero
