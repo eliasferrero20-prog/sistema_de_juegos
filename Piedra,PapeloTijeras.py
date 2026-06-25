091388c4fa9b090c0990989bea64698aa60b7b28
Miqueas17 <miqueasbarrozo12@gmail.com>
2026-06-25 17:35:32 -0300

    agrege while y hice el codigo mas corto

--- Piedra,PapeloTijeras.py
+++ Piedra,PapeloTijeras.py
@@ -1,25 +1,25 @@
 import random
 aleatorio=["piedra", "papel", "tijera" ]
+eleccion=True
 aleatorio2=random.choice(aleatorio)
-eleccion1=input("(Piedra, Papel o Tijera)")
-print(aleatorio2)
-if eleccion1 == "piedra" and aleatorio2 == "papel":
-    print("Ganador: Papel")
-elif eleccion1 == "tijera" and aleatorio2 == "papel":
-    print("Ganador: Tijera")
-elif eleccion1 == "piedra" and aleatorio2 == "tijera":
-    print("Ganador: Piedra")
-elif eleccion1 == "papel" and aleatorio2 == "piedra":
-    print("Ganador: Papel")
-elif eleccion1 == "papel" and aleatorio2 == "tijera":
-    print("Ganador: Tijera")
-elif eleccion1 == "tijera" and aleatorio2 == "piedra":
-    print("Ganador: Piedra")
-elif eleccion1 == "tijera" and aleatorio2 == "tijera":
-    print("Empate")
-elif eleccion1 == "piedra" and aleatorio2 == "piedra":
-    print("Empate")
-elif eleccion1 == "papel" and aleatorio2 == "papel":
-    print("Empate")
-else :
-    print("Dato ingresado no valido")
+while eleccion == True:
+    eleccion1=input("(Piedra, Papel o Tijera)")
+    eleccion1=eleccion1.lower()
+    print(aleatorio2)
+    if eleccion1 == "piedra" and aleatorio2 == "papel":
+        print("Ganador: Papel")
+    elif eleccion1 == "tijera" and aleatorio2 == "papel":
+        print("Ganador: Tijera")
+    elif eleccion1 == "piedra" and aleatorio2 == "tijera":
+        print("Ganador: Piedra")
+    elif eleccion1 == "papel" and aleatorio2 == "piedra":
+        print("Ganador: Papel")
+    elif eleccion1 == "papel" and aleatorio2 == "tijera":
+        print("Ganador: Tijera")
+    elif eleccion1 == "tijera" and aleatorio2 == "piedra":
+            print("Ganador: Piedra")
+    elif eleccion1 == aleatorio2:
+        print("Empate")
+    else :
+        print("Dato ingresado no valido")
+    eleccion=False
