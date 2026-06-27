166cf50937aab4101bfee233c88aa7dfd5937ddb
ggrossidev <grossigon@gmail.com>
2026-06-27 11:04:36 -0300

    Funciones ahorcado

--- ahorcado.py
+++ ahorcado.py
@@ -1,29 +1,48 @@
 import random
-palabra=["casa", "perro", "gato", "mesa", "silla", "ventana", "puerta", "escuela", "amigo", "pelota"]
-aleatorio=random.choice(palabra)
-palabra1=""
-for i in range(len(aleatorio)):
-    palabra1+="-"
-print(palabra1)
-palabrasecreta=""
-intentos=5
-print("Pista (primer letra):", aleatorio[0])
-finalizado=False
-while intentos!=0 and finalizado==False:
-    letra=input()
-    palabrasecreta=""
-    intentos-=1
-    for i in range(len(aleatorio)):
-        if letra==aleatorio[i]:
-            palabrasecreta+=letra
+
+def seleccionar_palabra_ahorcado():
+
+    palabras = ["casa", "perro", "gato", "mesa", "silla", "ventana", "puerta", "escuela", "amigo", "pelota"]
+    return random.choice(palabras)
+
+def mostrar_inicio_ahorcado(palabra):
+
+    palabra_oculta = "-" * len(palabra)
+    print(palabra_oculta)
+    print("Pista (primera letra):", palabra[0])
+    return palabra_oculta
+
+def jugar_ahorcado():
+    print("\n==============================")
+    print("     AHORCADO")
+    print("==============================")
+    palabra_aleatoria = seleccionar_palabra_ahorcado()
+    palabra_progreso = mostrar_inicio_ahorcado(palabra_aleatoria)
+    intentos = 5
+    finalizado = False
+    while intentos > 0 and finalizado == False:
+        letra = input("\nIngresa una letra: ").lower()
+        palabra_secreta = ""
+        intentos -= 1
+        acierto = False
+        for i in range(len(palabra_aleatoria)):
+            if letra == palabra_aleatoria[i]:
+                palabra_secreta += letra
+                acierto = True
+            else:
+                palabra_secreta += palabra_progreso[i]
+        if acierto == True:
             intentos += 1
-        else:
-            palabrasecreta+=palabra1[i]
-    palabra1=palabrasecreta
-    print(palabrasecreta)
-    if aleatorio == palabrasecreta:
-        finalizado=True
-if aleatorio==palabrasecreta:
-    print("Ganaste")
-else:
-    print("Perdiste")
+        palabra_progreso = palabra_secreta
+        print(palabra_progreso)
+        print("Intentos restantes:", intentos)
+        if palabra_aleatoria == palabra_progreso:
+            finalizado = True
+    if palabra_aleatoria == palabra_progreso:
+        print("\n¡Ganaste!")
+    else:
+
+        print("\nPerdiste. La palabra era: " + palabra_aleatoria)
+
+
+jugar_ahorcado()
