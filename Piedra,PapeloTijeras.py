8bb4a8a46a9b422ec0c4da53da9d35793324fc1f
Miqueas17 <miqueasbarrozo12@gmail.com>
2026-06-27 02:03:25 -0300

    agregue detalles y corregi un error del aleatorio

--- Piedra,PapeloTijeras.py
+++ Piedra,PapeloTijeras.py
@@ -1,30 +1,32 @@
 def piedrapapeltijeras():
     import random
     aleatorio=["piedra", "papel", "tijera" ]
-    opcion=2
-    aleatorio2=random.choice(aleatorio)
-    while opcion == 2:
+    opcion="2"
+    while opcion == "2":
         try:
-            eleccion1=input("(Piedra, Papel o Tijera)")
+            aleatorio2=random.choice(aleatorio)
+            eleccion1=input("\n(Piedra, Papel o Tijera) ")
             eleccion1=eleccion1.lower()
-            print(aleatorio2)
+            print("\n(Piedra, Papel o Tijera)", aleatorio2)
             if eleccion1 == "piedra" and aleatorio2 == "papel":
-                print("Ganador: Papel")
+                print("\nGanador: Papel")
             elif eleccion1 == "tijera" and aleatorio2 == "papel":
-                print("Ganador: Tijera")
+                print("\nGanador: Tijera")
             elif eleccion1 == "piedra" and aleatorio2 == "tijera":
-                print("Ganador: Piedra")
+                print("\nGanador: Piedra")
             elif eleccion1 == "papel" and aleatorio2 == "piedra":
-                print("Ganador: Papel")
+                print("\nGanador: Papel")
             elif eleccion1 == "papel" and aleatorio2 == "tijera":
-                print("Ganador: Tijera")
+                print("\nGanador: Tijera")
             elif eleccion1 == "tijera" and aleatorio2 == "piedra":
-                    print("Ganador: Piedra")
+                    print("\nGanador: Piedra")
             elif eleccion1 == aleatorio2:
-                print("Empate")
+                print("\nEmpate")
             else :
-                print("Dato ingresado no valido")
-            print("2 = Seguir", "\n0 = Salir del juego")
-            opcion = input("Ingresa una opcion del menu: ")
+                print("\nDato ingresado no valido")
+            print("\n2 = Seguir", "\n0 = Salir del juego")
+            opcion = input("\nIngresa una opcion del menu: ")
         except ValueError:
-            print("Dato ingresado no valido")
+            print("\nDato ingresado no valido")
+
+piedrapapeltijeras()
